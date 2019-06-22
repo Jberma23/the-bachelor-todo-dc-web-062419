3223d003fd2490abd3c0ccce91180095c72a9e2f
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-22 21:04:29 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -23,7 +23,14 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  list_from = []
+  data.each do |season, array|
+    array.each do |contestants|
+      contestants.each do |contestant, contestant_data|
+        if contestant_data == hometown
+        list_from << contestants["name"]
+      end
+      return list_from.length
 end
 
 def get_occupation(data, hometown)
